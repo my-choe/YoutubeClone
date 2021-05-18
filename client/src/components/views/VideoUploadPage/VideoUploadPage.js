3eafa38603e2d5d81b68b697caca16c61b11b3d9
my-choe <my_choe@naver.com>
2021-05-18 11:11:46 +0900

    Multer로 노드 서버에 비디오 저장하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -39,6 +40,26 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }
 
+    const onDrop = (files) => {
+        let formData = new FormData;
+        const config = {
+            header: {'content-type' : 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if(response.data.success){
+                    console.log(response.data);
+                } else{
+                    alert('비디오 업로드를 실패했습니다.');
+                }
+            })
+
+
+
+    }
+
 
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
@@ -51,13 +72,13 @@ function VideoUploadPage() {
                     { /* Drop Zone */}
 
                     <Dropzone
-                    onDrop
-                    multiple
-                    maxSize
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={800000000}
                     >
                         {({ getRootProps, getInputProps }) => (
                             <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display:'flex', alignItems: 'center', justifyContent:'center'}} {...getRootProps()}>
-                                <Input {...getInputProps()} />
+                                <input {...getInputProps()} />
                                 <Icon type="plus" style={{ fontSize:'3rem'}} />
                             </div>
                         )}
